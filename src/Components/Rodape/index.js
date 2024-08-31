3b5cd22a39790da738215cdc13548112015fed5e
Camilla <milla_mendes83@hotmail.com>
2024-08-31 14:30:49 -0300

    Consertando Responsividade

--- src/Components/Rodape/index.js
+++ src/Components/Rodape/index.js
@@ -6,6 +6,19 @@ export default function Rodape() {
     <footer className={styles.rodape}>
       <MarcaRegistrada />
       Desenvolvido por Camilla Mendes.
+
+      {/* <a href="https://github.com/camillamendess" target="_blank" rel="noreferrer">
+        <img src={github} alt="GitHub" />
+      </a>
+      <a href="https://www.linkedin.com/in/camillamendess/" target="_blank" rel="noreferrer">
+        <img src={linkedin} alt="LinkedIn" />
+      </a>
+      <a href="https://www.instagram.com/camillamendz/" target="_blank" rel="noreferrer">
+        <img src={instagram} alt="Instagram" />
+      </a>
+      <a href="h" target="_blank">
+        <img src={email} alt="Email" />
+      </a> */}
     </footer>
   );
 }
