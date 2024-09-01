94fc3485ba339915d17a99ae17ccd12187ddf502
Camilla <milla_mendes83@hotmail.com>
2024-09-01 14:26:47 -0300

    Desenvolvendo footer - Adicionando icones

--- src/Components/Rodape/index.js
+++ src/Components/Rodape/index.js
@@ -1,24 +1,24 @@
 import styles from "./rodape.module.css";
-import { ReactComponent as MarcaRegistrada } from "assets/marca_registrada.svg";
+import github from "../../assets/github.png";
+import insta from "../../assets/insta.png";
+import linke from "../../assets/linke.png";
+import email from "../../assets/email.png";
 
 export default function Rodape() {
   return (
     <footer className={styles.rodape}>
-      <MarcaRegistrada />
-      Desenvolvido por Camilla Mendes.
-
-      {/* <a href="https://github.com/camillamendess" target="_blank" rel="noreferrer">
-        <img src={github} alt="GitHub" />
+      <a href="mailto:milla_mendes83@hotmail.com" target="_blank" rel="noreferrer">
+        <img src={email} alt="Email" />
       </a>
       <a href="https://www.linkedin.com/in/camillamendess/" target="_blank" rel="noreferrer">
-        <img src={linkedin} alt="LinkedIn" />
+        <img src={linke} alt="LinkedIn" />
       </a>
       <a href="https://www.instagram.com/camillamendz/" target="_blank" rel="noreferrer">
-        <img src={instagram} alt="Instagram" />
+        <img src={insta} alt="Instagram" />
       </a>
-      <a href="h" target="_blank">
-        <img src={email} alt="Email" />
-      </a> */}
+      <a href="https://github.com/camillamendess" target="_blank" rel="noreferrer">
+        <img src={github} alt="GitHub" />
+      </a>
     </footer>
   );
 }
