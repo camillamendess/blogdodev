dd74770a873c70002cb22babd36c7d1f6956d682
Camilla <milla_mendes83@hotmail.com>
2024-09-06 23:27:55 -0300

    Ajustando Rodapé

--- src/Components/Rodape/index.js
+++ src/Components/Rodape/index.js
@@ -7,18 +7,23 @@ import email from "../../assets/email.png";
 export default function Rodape() {
   return (
     <footer className={styles.rodape}>
-      <a href="mailto:milla_mendes83@hotmail.com" target="_blank" rel="noreferrer">
-        <img src={email} alt="Email" />
-      </a>
-      <a href="https://www.linkedin.com/in/camillamendess/" target="_blank" rel="noreferrer">
-        <img src={linke} alt="LinkedIn" />
-      </a>
-      <a href="https://www.instagram.com/camillamendz/" target="_blank" rel="noreferrer">
-        <img src={insta} alt="Instagram" />
-      </a>
-      <a href="https://github.com/camillamendess" target="_blank" rel="noreferrer">
-        <img src={github} alt="GitHub" />
-      </a>
+      <div>
+        <p>© 2024 Camilla Mendes</p>
+      </div>
+      <div className={styles.logos}>
+        <a href="mailto:milla_mendes83@hotmail.com" target="_blank" rel="noreferrer">
+          <img src={email} alt="Email" />
+        </a>
+        <a href="https://www.linkedin.com/in/camillamendess/" target="_blank" rel="noreferrer">
+          <img src={linke} alt="LinkedIn" />
+        </a>
+        <a href="https://www.instagram.com/camillamendz/" target="_blank" rel="noreferrer">
+          <img src={insta} alt="Instagram" />
+        </a>
+        <a href="https://github.com/camillamendess" target="_blank" rel="noreferrer">
+          <img src={github} alt="GitHub" />
+        </a>
+      </div>
     </footer>
   );
 }
